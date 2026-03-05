4986015ac3bf1109cafc748058c9023ea47eea70
MarekSedlmayer <marek.sedlmayer@email.cz>
2026-03-05 21:26:58 +0100

    Update Player script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,10 +13,24 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+
+    }
+    void OnLook(InputValue input)
+    {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(input.Get<Vector2>());
 
         float zRotation = Mathf.Atan2(transform.position.y - mousePosition.y, transform.position.x - mousePosition.x) * Mathf.Rad2Deg;
-
         transform.rotation = Quaternion.Euler(0, 0, zRotation);
+
+    }
+
+    void OnFire()
+    {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+
+        if (Vector2.Distance(mousePosition, new Vector2(transform.position.x, transform.position.y)) > 1f)
+        {
+            //Activate bullet
+        }
     }
 }
